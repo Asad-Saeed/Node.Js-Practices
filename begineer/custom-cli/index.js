5903d8bca65a3d875f7e896fff7bc35d20e0f08b
Asad Saeed <asadsaeed12225@gmail.com>
2024-01-18 22:15:55 +0500

    fear:how to intract with cli

--- begineer/custom-cli/index.js
+++ begineer/custom-cli/index.js
@@ -3,9 +3,24 @@
 // hashbang and shibang tell operating system which interprator to use here node is an interprator
 // enter npm install -g    to create our cli
 
-const yargs = require("yargs");
-const { argv } = yargs(process.argv);
+// // How to pass options to a cli
+// const yargs = require("yargs");
+// const { argv } = yargs(process.argv);
 
+// const printFivePokemonMoves = async (pokemonName) => {
+//   const apiResponse = await fetch(
+//     `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+//   );
+//   const pokemon = await apiResponse.json();
+//   const moves = pokemon.moves.map(({ move }) => move.name);
+//   console.log(moves.slice(0, 5));
+// };
+// printFivePokemonMoves(argv.pokemon);
+
+// How to Intract to a cli
+// install inquirer @8.2.5
+
+const inquirer = require("inquirer");
 const printFivePokemonMoves = async (pokemonName) => {
   const apiResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
@@ -14,4 +29,16 @@ const printFivePokemonMoves = async (pokemonName) => {
   const moves = pokemon.moves.map(({ move }) => move.name);
   console.log(moves.slice(0, 5));
 };
-printFivePokemonMoves(argv.pokemon);
+const prompt = inquirer.createPromptModule();
+// qestion like array of objects
+prompt([
+  {
+    type: "input",
+    name: "pokemon",
+    message: "Enter a pokemon name to view first 5 move:",
+  },
+]).then((answer) => {
+  const pokemon = answer.pokemon;
+  printFivePokemonMoves(pokemon);
+});
+
