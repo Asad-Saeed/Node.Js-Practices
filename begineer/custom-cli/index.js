cda39358ae7d1f05d8afe4acee572e9dc184b336
Asad Saeed <asadsaeed12225@gmail.com>
2024-01-18 22:03:25 +0500

    fear:how to add option to cli

--- begineer/custom-cli/index.js
+++ begineer/custom-cli/index.js
@@ -1,4 +1,17 @@
-#!/usr/bin/env node  
-console.log("Asad Pokemon")
+#!/usr/bin/env node
+// console.log("Asad Pokemon")
 // hashbang and shibang tell operating system which interprator to use here node is an interprator
 // enter npm install -g    to create our cli
+
+const yargs = require("yargs");
+const { argv } = yargs(process.argv);
+
+const printFivePokemonMoves = async (pokemonName) => {
+  const apiResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+  const pokemon = await apiResponse.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, 5));
+};
+printFivePokemonMoves(argv.pokemon);
