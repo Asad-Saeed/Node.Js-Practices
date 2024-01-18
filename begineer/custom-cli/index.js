7fa012e6f67cc0ade66ea32b06032531d5acb22a
Asad Saeed <asadsaeed12225@gmail.com>
2024-01-18 21:45:24 +0500

    fear:how to create a cli

---
+++ begineer/custom-cli/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node  
+console.log("Asad Pokemon")
+// hashbang and shibang tell operating system which interprator to use here node is an interprator
+// enter npm install -g    to create our cli
